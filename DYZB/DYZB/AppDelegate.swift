14ec586a6d23b38612a9f81ab6f5172a72d7ffbc
GuoMingJian <GuoMJ158@163.com>
2017-09-11 17:44:19 +0800

    info

--- DYZB/DYZB/AppDelegate.swift
+++ DYZB/DYZB/AppDelegate.swift
@@ -12,6 +12,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    //基金2
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
