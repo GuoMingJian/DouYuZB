ea4516df667e3a9e69007fe4446134a6c6a5b47a
GuoMingJian <GuoMJ158@163.com>
2017-09-13 16:52:59 +0800

    create viewcontroller

---
+++ DYZB/DYZB/Classes/Profile/ProfileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ProfileViewController.swift
+//  DYZB
+//
+//  Created by 郭明健 on 2017/9/13.
+//  Copyright © 2017年 com.joy.www. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DYZB/DYZB/Classes/Tools/MJTools.swift
@@ -0,0 +1,13 @@
+//
+//  MJTools.swift
+//  DYZB
+//
+//  Created by 郭明健 on 2017/9/13.
+//  Copyright © 2017年 com.joy.www. All rights reserved.
+//
+
+import UIKit
+
+class MJTools: NSObject {
+
+}
